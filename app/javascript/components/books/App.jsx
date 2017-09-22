bf4bd01389846d230175f5ea5d8a5fdda8c67b81
Victor Feijo <victor.feijoa@gmail.com>
2017-09-22 11:46:12 -0300

    Styled components

--- app/javascript/components/books/App.jsx
+++ app/javascript/components/books/App.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { toUpper } from 'ramda';
+
+const SearchCnt = styled.div`
+  border: 2px solid black;
+  margin: 20px;
+  display: flex;
+  height: 100px;
+`;
 
 class App extends Component {
   render () {
     const { name } = this.props;
 
     return (
-      <div> Application root { toUpper(name) } </div>
+      <div>
+        Application root { toUpper(name) }
+        <SearchCnt />
+      </div>
     );
   }
 }
